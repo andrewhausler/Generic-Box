007d144c07130d098286e643eebfb5a14cc40873
Andrew Hausler <81341702+andrewhausler@users.noreply.github.com>
2024-02-13 16:32:03 -0500

    New Files Added

---
+++ Box.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+
+public abstract class Box<Type extends Comparable<Type>>
+{
+    private int width; // width is equal to rows
+    private int height; // height is equal to columns
+    private ArrayList<ArrayList<Type>> box;
+    
+    /**
+     * Sets the size of the box and each index is set to null
+     * @param width
+     * @param height
+     */
+    public Box(int width, int height)
+    {
+        if(width <= 0 || height <= 0)
+        {
+            throw new IllegalArgumentException();
+        }
+        this.height = height;
+        this.width = width;
+        for(int i=0; i<width; i++)
+        {
+            box.add(new ArrayList<Type>());
+            for(int j=0; j<height; j++)
+            {
+                box.get(i).add(null);
+            }
+        }
+    }
+
+    public int getWidth()
+    {
+        return this.width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public ArrayList<ArrayList<Type>> getBox()
+    {
+        return box;
+    }
+
+    public void putInBox(Type value, int row, int column)
+    {
+        try
+        {
+            box.get(row).add(column, value);
+        }
+        catch(IndexOutOfBoundsException ex)
+        {
+            throw new IndexOutOfBoundsException("There is no room over there!!!");
+        }
+    }
+
+    public void displayBox()
+    {
+        for(int i=0; i<box.size(); i++)
+        {
+            for(int j=0; j<box.get(i).size(); i++)
+            {
+                if(box.get(i).get(j) == null)
+                {
+                    System.out.print("#");
+                }
+                else
+                {
+                    System.out.print(box.get(i).get(j));
+                }
+            }
+        }
+    }
+}
---
+++ RectangularBox.java
@@ -0,0 +1,8 @@
+public class RectangularBox<Type extends Comparable<Type>> extends Box<Type>
+{
+    public RectangularBox(int width, int height)
+    {
+        super(width, height);
+    }
+
+}
